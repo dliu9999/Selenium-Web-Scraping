232440363743c4117395ce7d1cb5d4546e25e666
Darren <dliu9999@gmail.com>
2018-08-18 15:46:07 -0700

    added selenium options

--- vons_test.py
+++ vons_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as soup
 import time
